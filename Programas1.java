e83993a9db0be8f88515c8f40e4749c64f80b1cd
Fabio dos Santos Mota <96454065+FabioMota30@users.noreply.github.com>
2022-03-14 10:30:18 -0300

    Add files via upload

---
+++ Programas1.java
@@ -0,0 +1,17 @@
+package exemplos.exercicios;
+
+public class Programas1 {
+
+	public static void main(String[] args) {
+		int idade = 18;
+		String nome = "Zico";
+		char sexo = 'M';
+		double salario = 1870.30;
+		boolean aposentado = true;
+		
+		System.out.print(" O Sr. :" + nome + " tem "+ idade + " é de idade" + " do sexo " + sexo + " ganha R$ " + salario + " e é aposentado " + aposentado);
+
+
+	}
+
+}
